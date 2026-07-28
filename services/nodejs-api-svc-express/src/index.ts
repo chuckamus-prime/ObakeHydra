b9bfa64361f14d02edd3f84c3b2e9da3840405b4
chuckamus-prime <chucksailors.software@outlook.com>
2026-07-28 11:17:30 -0400

    made  the express server subscribe to a dapr topic, then publish to the topic on an interval for demonstration purposes. made a helper to create the daprServer. made the nodejs-api-svc-express service in docker-compose.yaml have the dapr env vars set for convenient overriding.

--- services/nodejs-api-svc-express/src/index.ts
+++ services/nodejs-api-svc-express/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import healthRouter from './routes/health';
+import { CreateDaprGrpcClient, CreateDaprServer } from './utils/dapr';
+import { Logger } from '@dapr/dapr/logger/Logger';
 
 console.log("service starting...");
 const app = express();
@@ -9,5 +11,43 @@ const port = process.env.HTTP_PORT ?? 5002;
 app.use('/api/health', healthRouter);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Express server is running on port ${port}`);
+});
+const pubsubName = "pubsub";//"pmtool-pubsub-project-attribute-changed"
+const topicName = "project.attribute.changed";
+const publishTurnedOn = true;
+let daprServer = CreateDaprServer();
+let daprClient = CreateDaprGrpcClient();
+daprServer.pubsub.subscribe(pubsubName, topicName, async (data) => {
+    console.debug(`Received event from Dapr: ${data}`);
+}).then(() => {
+    console.log(`Subscribed to ${topicName} topic`);
+    return daprServer.start();
+}).then(() => {
+    console.log(`Dapr server started`);
+    //now try to publish some events in a loop
+    if (publishTurnedOn){
+    setInterval(async () => {
+      //generate a dummy id
+      let id = Math.floor(Math.random() * 1000000);
+        const eventData = {
+            Id: id.toString(),
+            attributeName: "status",
+            newValue: "active",
+            timeStamp: new Date().toISOString()
+        };
+        try {
+            const daprResponse = await daprClient.pubsub.publish(pubsubName, topicName, eventData);
+            if (daprResponse.error) {
+                  throw new Error(`Dapr publish error: ${daprResponse.error}`);
+            }
+            console.info(`Publishing event to Dapr: ${eventData.attributeName} for id ${eventData.Id}`);
+        } 
+        catch (error) {
+            console.error("Error publishing event:", error);
+        }
+    }, 1000);
+  }
+}).catch((error) => {
+    console.error("Error subscribing to topic:", error);
 });
--- services/nodejs-api-svc-express/src/utils/dapr/index.ts
+++ services/nodejs-api-svc-express/src/utils/dapr/index.ts
@@ -1,10 +1,13 @@
-import {CommunicationProtocolEnum, DaprClient} from "@dapr/dapr";
+import {CommunicationProtocolEnum, DaprClient, DaprServer} from "@dapr/dapr";
 import {DaprClientOptions} from "@dapr/dapr/types/DaprClientOptions";
 
 const daprHost = process.env.DAPR_HOST ?? "localhost";
+const daprHttpPort = process.env.DAPR_HTTP_PORT ?? "50002";
+const darGrpcPort = process.env.DAPR_GRPC_PORT ?? "50001";
+const serverPort = process.env.DAPR_SERVER_PORT ?? "6000";
 
-function  CreateDaprHttpClient() {
-    const daprHttpPort = process.env.DAPR_HTTP_PORT ?? "50002";
+function  CreateDaprHttpClient() {    
+    console.debug(`Creating Dapr HTTP client with host: ${daprHost}, port: ${daprHttpPort}`);
     let options:DaprClientOptions = {
         daprHost: daprHost,
         daprPort: daprHttpPort,
@@ -14,7 +17,6 @@ function  CreateDaprHttpClient() {
 }
 
 function CreateDaprGrpcClient() {
-    const darGrpcPort = process.env.DAPR_GRPC_PORT ?? "50001";
     let options:DaprClientOptions = {
         daprHost: daprHost,
         daprPort: darGrpcPort,
@@ -23,5 +25,19 @@ function CreateDaprGrpcClient() {
    return new DaprClient(options);
 }
 
+function CreateDaprServer(){
+    console.debug(`Creating Dapr server with host: ${daprHost}, port: ${serverPort}`);
+    let daprServer = new DaprServer({
+      serverHost: daprHost,
+      serverPort,
+      communicationProtocol: CommunicationProtocolEnum.HTTP,
+      clientOptions: {
+        daprHost,
+        daprPort: daprHttpPort,
+      },
+    });
+    return daprServer;
+}
 
-export { CreateDaprHttpClient, CreateDaprGrpcClient };
+
+export { CreateDaprHttpClient, CreateDaprGrpcClient, CreateDaprServer };
